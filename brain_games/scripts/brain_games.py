ec34772b0489b94e6295067dcad45811a28816bb
Irina Kulbak <melisandra29@gmail.com>
2021-02-26 17:55:51 +0300

    step1

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Brain games welcome game."""
+
+
+def main():
+    print( """Welcome user without running a game.""")
+
+
+
+if __name__ == '__main__':
+    main()
