30a189c4d849a24fa5bc0d314b81bed354fd9440
irina <34609502+Melisandra29@users.noreply.github.com>
2022-01-25 18:08:22 +0300

    Update les_hexlet_22.py

--- les_hexlet_22.py
+++ les_hexlet_22.py
@@ -9,3 +9,11 @@ def filter_string(my_string, element):
     while '  ' in result:
       result = result.replace('  ', ' ')
     return result
+#Решение учителя
+def filter_string(text, char):
+    result = ''
+    lowered_char = char.lower()
+    for current_char in text:
+        if current_char.lower() != lowered_char:
+            result += current_char
+    return result.strip().replace('  ', ' ')
